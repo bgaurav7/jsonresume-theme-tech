6c6aa1616d8b0add7440303f0ba4edfc027f8cf4
Gaurav Bansal <gauravbansal9999@gmail.com>
2026-03-25 03:47:32 +0530

    Refactor resume rendering logic by removing course column splitting and updating Handlebars template for improved layout. Enhance CSS for better readability and consistency, including adjustments to font sizes and line heights.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
 
-COURSES_COLUMNS = 3;
-
 PREPEND_SUMMARY_CATEGORIES = [
   "work",
   "volunteer",
@@ -15,27 +13,6 @@ function validateArray(arr) {
 }
 
 function render(resume) {
-  // Split courses into 3 columns
-  if (validateArray(resume.education)) {
-    resume.education.forEach(function(block) {
-      if (validateArray(block.courses)) {
-        splitCourses = [];
-        columnIndex = 0;
-        for (var i = 0; i < COURSES_COLUMNS; i++) {
-          splitCourses.push([]);
-        }
-        block.courses.forEach(function(course) {
-          splitCourses[columnIndex].push(course);
-          columnIndex++;
-          if (columnIndex >= COURSES_COLUMNS) {
-            columnIndex = 0;
-          }
-        });
-        block.courses = splitCourses;
-      }
-    });
-  }
-
   PREPEND_SUMMARY_CATEGORIES.forEach(function(category) {
     if (resume[category] !== undefined) {
       resume[category].forEach(function(block) {
